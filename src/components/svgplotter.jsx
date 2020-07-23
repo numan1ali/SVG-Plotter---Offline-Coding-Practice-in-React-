4239f722538cdc88b9dfdc02a460f520eb6ccc04
numan1ali <33228347+numan1ali@users.noreply.github.com>
2020-07-24 03:49:33 +0500

    Removed unwanted code

--- src/components/svgplotter.jsx
+++ src/components/svgplotter.jsx
@@ -1,27 +1,16 @@
 import React, { Component } from "react";
 
 class Plotter extends Component {
-  //   state = {
-  //     count: 0,
-  // tags: [],
-  /*imageUrl: 'https://picsum.photos/200'*/
-  //   };
   constructor() {
     //   Counter is a child class as we doing inheritance here, it is extending Component class. So, before calling the
     //   constructor of child class we have to call the constructor of parent class and we do that by calling super().
     super();
-    //   console.log("Constructor", this);
-    // Functions in javascript are objects, So they can have properties and methods, bind() is one of them.
-    // and with bind we can set the value of this. bind() method will return the new instance of count increment function
-    // and in that function this will be refering to the current object.
-    // So, it will return a new function and we will be setting it to the countIncrement function.
     this.countIncrement = this.countIncrement.bind(this);
     this.state = { input: "", count: 0, flag: false };
     this.onSubmit = this.onSubmit.bind(this);
-    this.drawCicle = this.drawCicle.bind(this);
   }
   // its good practice to make class for styling keeping in the view
-  // maintainbility and performance but sometimes we may want to break the rules so,
+  // maintainbility and performance.
   styles = {
     fontSize: 15,
     fontWeight: "bold",
@@ -78,29 +67,6 @@ class Plotter extends Component {
     },
   };
 
-  renderTags() {
-    /*
-        Unlike angular we don't have if/else condition because its not 
-        a templating engine. So, to render elements conditionally we need to go back to 
-        plain old javascript.
-    */
-    //   if Array is empty then it will render "there is no tags."
-    if (this.state.tags.length === 0) return <p>There are no Tags!</p>;
-
-    // otherwise this will get render.
-    return (
-      <ul>
-        {/* Iterators we use in javascript are Map(ES6), ForEach, For-of, among them Map is best to use.
-            To ensure that each HTML element in the React DOM has a unique 
-            identifier, you’re required to provide each rendered element with, well, a unique key.
-            You do this by providing a key attribute on the HTML element that 
-            you are rendering inside of the Map function. */}
-        {this.state.tags.map((tag, index) => (
-          <li key={index}>{tag}</li>
-        ))}
-      </ul>
-    );
-  }
   generateRandomColor() {
       let color = Math.floor(Math.random()*16777215).toString(16);
       return "#" + color;
@@ -108,18 +74,13 @@ class Plotter extends Component {
 
   onSubmit(e) {
     e.preventDefault();
-    // this.state.input = this.title;
+
     let text = document.getElementById("svgInput");
     this.isPolygon = false;
     this.isEllipse = false;
     this.setState({ flag: false });
     let values = text.value.replace(/\r\n/g, "\n").split("\n");
-    let check = values.toString();
-    check.split("");
-    console.log(this.state.input);
-    console.log(values);
-    // console.log(check.split(' '));
-    // console.log(this.arrayToStoreValues);
+
     if (values.length > 0) {
       values.forEach((element) => {
         let convertFromArrayToString = element.toString();
@@ -157,12 +118,6 @@ class Plotter extends Component {
       console.log(values);
       console.log(this.arrayToStoreValues);
     } else console.log("Please enter input");
-    // if(this.arrayToStoreValues[0] === 'c') {
-    //     this.xCoordinat = parseInt(this.arrayToStoreValues.circle[1]);
-    //     this.yCoordinat = parseInt(this.arrayToStoreValues.circle[2]);
-    //     this.radius = parseInt(this.arrayToStoreValues.circle[3]);
-    //     this.setState({flag:true});
-    // }
   }
   myChangeHandler = (event) => {
     this.setState({ input: event.target.value });
@@ -173,60 +128,10 @@ class Plotter extends Component {
     let ellipse = this.isEllipse;
     return (
       <div>
-        {/* <React.Fragment> use this tag if you want to render something without using any tag */}
-        {/* <img src={this.state.imageUrl} alt=""/ > */}
-        {/* <span style={this.styles} className="badge badge-primary m-2">{this.state.count } </span> */}
-        {/* for inline styling style={{ fontSize: 20}} */}
-        {/*<span className="badge badge-primary m-2">{this.state.count } </span> */}
-
-        {/* 
-            Imagine just before rendering the rnederTags method we want to render another
-            method based on the condition and we have simple IF statement without and else part.
-            So, this is how we will be achieving this in React 
-          */}
-        {/* 
-            How this is actually works, in javascript unlike other programming languages we can apply 
-            logical (&&) and operator between non-boolean values.
-            Examples, Plain javascript.
-            ----->: true && false
-            result: flase
-
-            what will be the result?
-            ----->: true && 'Hi'
-            result: Hi
-            Reason: When javascript tries to eveluate this expression, it will look at the first operand 
-                    and in this case its true so it will look at the second operand and Its a string and not empty (string which have atleast one character always return true)
-                    so it will also give us true. Essentially we have two operands, which are truthy.
-                    In that case Javascript engine return the second operand, that's why we get the 'Hi'
-            
-            ----->: true && 'Hi' && 1
-            result: Hi
-            Reason: All three operands are true, if number is not 0 then it will always be true. So, the result is the last operand, we will get 1. 
-
-            Ps: First will be the logical condition and then it could be any jsx express.
-          */}
-        {/* { this.state.tags.length === 0 && 'Please create a new tag!'}
-          {this.renderTags()} */}
-
-        {/* <span className={this.getBadgeClasses()}>{this.formatCount()} </span> */}
-
-        {/* 
-            Note that, we are not calling the method, we are simply passing refrence to it.
-            this is different from handling onclick in Vanila Javascript, in vanila JS we call
-            the target function, when setting the onclick.
-        */}
-        {/* <button
-          onClick={this.countIncrement}
-          className="btn btn-secondary btn-sm"
-        >
-          Increment
-        </button> */}
-
         <div style={{ margin: 20, textAlign: "center" }}>
           <form>
             <h1>SVG Plotter</h1>
             <p>Enter Values:</p>
-            {/* <input type="text" onChange={this.myChangeHandler} /> */}
             <textarea
               id="svgInput"
               name="textarea"
@@ -251,7 +156,7 @@ class Plotter extends Component {
                     />
                   </svg>
                 )}
-              {/* { this.button? <span>{this.drawCicle}</span>:null} */}
+
             </div>
             <div style={this.gridItem}>
               {this.arrayToStoreValues.rectangle.values.length === 5 &&
@@ -300,63 +205,11 @@ class Plotter extends Component {
                 )}
             </div>
           </div>
-          {/* <span>
-            { this.arrayToStoreValues.length === 5 && this.state.flag && }
-        </span> */}
-          {/* <svg height="250" width="250">
-            {this.arrayToStoreValues.map((item,index)=>{
-                return(
-                    <circle
-            cx={item[]}
-            cy="30"
-            r="20"
-            stroke="black"
-            stroke-width="3"
-            fill="red"
-          />
-                )
-            })}
-          
-        </svg> */}
         </div>
       </div>
     );
   }
-  drawCicle() {
-    return (
-      <div>
-        <svg height="250" width="250">
-          {this.arrayToStoreValues.circle.values.length > 0 &&
-            this.state.flag && (
-              <circle
-                cx={this.arrayToStoreValues.circle.xCoordinat}
-                cy={this.arrayToStoreValues.circle.yCoordinat}
-                r={this.arrayToStoreValues.circle.radius}
-                style={{ stroke: "black", strokeWidth: 3, fill: "red" }}
-              />
-            )}
-        </svg>
-      </div>
-    );
-  }
 
-  countIncrement() {}
-  getBadgeClasses() {
-    // Using a let variable instead of const because we cannot change the state of const variable
-    // but we can change the field of const object e.g
-    // const obj={ name:'Ali'};
-    // obj.name="Numan";
-
-    // Rendering class properties dynamically
-    let classes = "badge m-2 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
-    return classes;
-  }
-
-  formatCount() {
-    const { count } = this.state;
-    return count === 0 ? "Zero" : count;
-  }
 }
 
 export default Plotter;
